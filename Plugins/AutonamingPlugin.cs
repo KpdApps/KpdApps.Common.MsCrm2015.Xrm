c6ed58599dde7cc658719186afac15fc142eb9c4
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-10-25 12:19:21 +0300

    * project init

---
+++ Plugins/AutonamingPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KpdApps.Common.MsCrm2015.Xrm.Plugins
+{
+    public class AutoNamingPlugin : BasePlugin
+    {
+        public override void ExecuteInternal(PluginState state)
+        {
+            
+        }
+    }
+}

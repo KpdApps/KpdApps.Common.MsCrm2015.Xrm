13de453955b603ecc53dbf976d43390f854b7ea6
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-10-31 14:10:09 +0300

    #1 refactoring for Aotugenerating plugins - parent class with base logic

---
+++ Plugins/AutogeneratingBasePlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Xrm.Sdk;
+
+namespace KpdApps.Common.MsCrm2015.Xrm.Plugins
+{
+    abstract public class AutogeneratingBasePlugin : BasePlugin
+    {
+        protected Entity TargetEntity;
+        protected string TargetEntityName;
+        protected IOrganizationService Service;
+
+
+        protected void InitTargetAndService(PluginState state)
+        {
+            TargetEntity = state.GetTarget<Entity>();
+            TargetEntityName = TargetEntity.LogicalName;
+            Service = state.Service;
+        }
+
+        // Возвращаемое значение делигата должно быть типа стринг.
+        private readonly Dictionary<string, Delegate> predefinedFunctions = new Dictionary<string, Delegate>
+        {
+            { "date", new Func<string, string>(GetCurrentDateString) }
+        };
+
+        protected string GenerateValueByMask(string mask)
+        {
+            MatchCollection matches = Regex.Matches(mask, @"\{(.+?)\}");
+
+            List<string> resultMatches = (from Match m in matches select m.Groups[1].ToString()).ToList();
+
+            var results = new Dictionary<string, string>();
+
+            foreach (string resultMatch in resultMatches)
+            {
+                var resultValue = string.Empty;
+                if (resultMatch.IndexOf(':') > -1)
+                {
+                    string[] parts = resultMatch.Split(':');
+
+                    if (!predefinedFunctions.ContainsKey(parts[0]))
+                        throw new InvalidPluginExecutionException($"Попытка вызова неопределенной функции {parts[0]} для сущности {TargetEntityName}");
+
+                    resultValue = (string)predefinedFunctions[parts[0]].DynamicInvoke(parts[1]);
+                }
+                else
+                {
+                    resultValue = ProcessField(resultMatch);
+                }
+                results.Add("{" + resultMatch + "}", resultValue.ToString());
+            }
+
+            var resultString = mask;
+            foreach (var result in results)
+            {
+                resultString = resultString.Replace(result.Key, result.Value);
+            }
+
+            return resultString;
+        }
+
+        protected void AddPredefinedFunction(string functionName, Delegate function)
+        {
+            predefinedFunctions.Add(functionName, function);
+        }
+
+        private static string GetCurrentDateString(string format)
+        {
+            return DateTime.Now.ToString(format);
+        }
+
+        abstract protected string ProcessField(string resultMatch);
+    }
+}
--- Plugins/AutonamingPlugin.cs
+++ Plugins/AutonamingPlugin.cs
@@ -1,16 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using KpdApps.Common.MsCrm2015.Extensions;
+using KpdApps.Common.MsCrm2015.Helpers;
 
 namespace KpdApps.Common.MsCrm2015.Xrm.Plugins
 {
-    public class AutoNamingPlugin : BasePlugin
+    public class AutonamingPlugin : AutogeneratingBasePlugin
     {
         public override void ExecuteInternal(PluginState state)
         {
-            
+            InitTargetAndService(state);
+            Entity autonaming = GetAutonamingByEntityName(TargetEntityName, Service);
+
+            if (autonaming == null)
+                return;
+
+            string mask = autonaming.Attributes.GetStringValue(Schema.Autonaming.Mask);
+            string generatedValue = GenerateValueByMask(mask);
+
+            string fieldName = autonaming.Attributes.GetStringValue(Schema.Autonaming.FieldName);
+            TargetEntity.Attributes.SetStringValue(fieldName, generatedValue);
+        }
+
+        private static Entity GetAutonamingByEntityName(string entityName, IOrganizationService service)
+        {
+            QueryExpression query = new QueryExpression
+            {
+                EntityName = Schema.Autonaming.LogicalName,
+                NoLock = true,
+                Distinct = true,
+                ColumnSet = new ColumnSet(Schema.Autonaming.Id, Schema.Autonaming.FieldName, Schema.Autonaming.Mask),
+                Criteria =
+                {
+                    Conditions =
+                    {
+                        new ConditionExpression(Schema.Autonaming.EntityName, ConditionOperator.Equal, entityName)
+                    }
+                }
+            };
+
+            EntityCollection result = service.RetrieveMultiple(query);
+
+            if (result.Entities.Any())
+                return result.Entities[0];
+
+            return null;
+        }
+
+        protected override string ProcessField(string resultMatch)
+        {
+            string fieldAddress = resultMatch;
+
+            var value = CrmFieldHelper.GetFieldValueByFieldAddress(TargetEntity, fieldAddress, Service);
+
+            return value.ToString();
         }
     }
 }

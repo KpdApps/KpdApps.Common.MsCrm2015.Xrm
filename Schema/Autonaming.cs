e9e6ecd1bfa9e4d4232581b40c1c7fdbf5c98b59
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-10-31 14:09:06 +0300

    #1 enity schemas

---
+++ Schema/Autonaming.cs
@@ -0,0 +1,11 @@
+﻿namespace KpdApps.Common.MsCrm2015.Xrm.Schema
+{
+    public class Autonaming
+    {
+        public const string EntityName = "kpd_entityname";
+        public const string FieldName = "kpd_fieldname";
+        public const string Mask = "kpd_mask";
+        public const string Id = "kpd_autonamingid";
+        public const string LogicalName = "kpd_autonaming";
+    }
+}
---
+++ Schema/Autonumbering.cs
@@ -0,0 +1,12 @@
+﻿namespace KpdApps.Common.MsCrm2015.Xrm.Schema
+{
+    public class Autonumbering
+    {
+        public const string EntityName = "kpd_entityname";
+        public const string FieldName = "kpd_fieldname";
+        public const string Mask = "kpd_mask";
+        public const string Counter = "kpd_counter";
+        public const string Id = "kpd_autonumberingid";
+        public const string LogicalName = "kpd_autonumbering";
+    }
+}
